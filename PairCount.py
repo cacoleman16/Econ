d7fe9fdeae9086a507f4985567706919314f083c
cacoleman16 <60491004+cacoleman16@users.noreply.github.com>
2020-02-25 13:17:50 -0500

    Update PairCount.py

--- PairCount.py
+++ PairCount.py
@@ -1,26 +1,33 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+import re
 
-## Creating Map Reduce Job for HW1 in CS626 using MRJOB
+Word_RE = re.compile(r"[\w']+")
+
+
 class myFirstMR(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_unique_pairs,
-                   reducer=self.reducer_count_combos)  # Come back and place in the mapper and reducer)
+                   reducer=self.reducer_count_combos) 
 
         ]
-# Here I order each pair of the data from the CSV file.
 
     def mapper_get_unique_pairs(self, _, line):
-        pair = line.split(',')
+        pair = Word_RE.findall(line)
         if pair[1] > pair[0]:
             pair[0], pair[1] = pair[1], pair[0]
+
        # (c1, c2) = line.split(',')
         yield (pair, 1)
-# Sum up the number of occurances for each unique pair 
+
+     #   for word in Word_RE.findall(line):
+     #       yield (word.lower(), 1)
+
     def reducer_count_combos(self, pair, values):
+        self.increment_counter('group', 'unique pairs', 1)
         yield pair, sum(values)
 
-## Just so it only runs if I am directly running this script
+
 if __name__ == '__main__':
     myFirstMR.run()
